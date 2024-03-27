c90dfc5737c40f98139c7aa51f66bccbf0f6a8b5
mickeymond <33749130+mickeymond@users.noreply.github.com>
2024-03-27 20:05:22 +0000

    Uploading files to savefilesorg

--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -1,9 +1,22 @@
 import { Router } from "express";
 import multer from "multer";
 import { addRecipe, deleteRecipe, getRecipe, getRecipes, updateRecipe } from "../controllers/recipes.controller.js";
+import { MulterSaveFilesOrgStorage } from "multer-savefilesorg-storage";
+import dotenv from "dotenv";
+
+// Load env variables
+dotenv.config({ path: ['.env.local'] });
 
 // Configure upload middleware
-const upload = multer({ dest: 'uploads/images' });
+// const upload = multer({ dest: 'uploads/images' });
+const upload = multer({
+    storage: MulterSaveFilesOrgStorage({
+        serverPath: `https://savefiles.org/api/v1/uploads`,
+        apiAccessToken: process.env.SAVEFILESORG_API_KEY,
+        relativePath: '/recipes/images/*'
+    }),
+    preservePath: true
+});
 
 // Create recipes router
 const router = Router();
